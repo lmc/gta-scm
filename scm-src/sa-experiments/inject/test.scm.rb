e79dc2730ddaac0a81de77ee629e2b08c7f9efec
Luke Mcildoon <luke@twofiftyfive.net>
2016-12-28 23:03:04 +1100

    reload code by jumping from old main loop to new

--- scm-src/sa-experiments/inject/test.scm.rb
+++ scm-src/sa-experiments/inject/test.scm.rb
@@ -1,8 +1,11 @@
 
+counter = 0
+
 routines do
 
   inject_init = routine(export: :inject_init) do
     wait(0)
+    counter = 123
   end
   
   inject_exit = routine(export: :inject_exit) do
@@ -10,11 +13,12 @@ routines do
   end
   
   inject_loop = routine(export: :inject_loop) do
+    counter += 1
     use_text_commands(1)
-    set_text_colour(0,100,255,255)
+    set_text_colour(0,255,0,255)
     set_text_scale(5.0,5.0)
     set_text_font(3)
-    display_text_with_number(100.0,100.0,"NUMBER",420)
+    display_text_with_number(100.0,100.0,"NUMBER",counter)
   end
   
 end
