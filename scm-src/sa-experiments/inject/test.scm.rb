0e9cbdfd82eff054c0fb488559cae6dd8672caec
Luke Mcildoon <luke@twofiftyfive.net>
2016-12-28 22:25:57 +1100

    more script reloader

--- scm-src/sa-experiments/inject/test.scm.rb
+++ scm-src/sa-experiments/inject/test.scm.rb
@@ -2,22 +2,19 @@
 routines do
 
   inject_init = routine(export: :inject_init) do
-    add_one_off_sound(0.0,0.0,0.0,1057)
-    wait(500)
-    add_one_off_sound(0.0,0.0,0.0,1058)
-    wait(500)
-    add_one_off_sound(0.0,0.0,0.0,1057)
-    wait(500)
+    wait(0)
   end
   
   inject_exit = routine(export: :inject_exit) do
-    add_one_off_sound(0.0,0.0,0.0,1058)
-    add_one_off_sound(0.0,0.0,0.0,1058)
+    wait(0)
   end
   
   inject_loop = routine(export: :inject_loop) do
-    add_one_off_sound(0.0,0.0,0.0,1057)
-    wait(250)
+    use_text_commands(1)
+    set_text_colour(0,100,255,255)
+    set_text_scale(5.0,5.0)
+    set_text_font(3)
+    display_text_with_number(100.0,100.0,"NUMBER",420)
   end
   
 end
